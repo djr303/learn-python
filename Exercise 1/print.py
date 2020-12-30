53794f864dcebb7d82bbe974c2ecdac248daebf9
david589 <david@infogrid.io>
2020-12-30 14:06:03 +0000

    * Exercises 1 - 17 complete

--- Exercise 1/print.py
+++ Exercise 1/print.py
@@ -1,4 +1,4 @@
-print "Hello World"
-print "Honestly Hello World"
-print "I'm kind hoping this is going to get interesting"
-print "We'll just have to see"
+print("Hello World")
+print("Honestly Hello World")
+print("I'm kind hoping this is going to get interesting")
+print("We'll just have to see")
---
+++ Exercise 2/comments.py
@@ -0,0 +1,4 @@
+# This is a comment nothing will happen herer 1 / 0
+print('I could have code like this')
+# Here's another comment. NOPE nothing
+print('YEP this works')
